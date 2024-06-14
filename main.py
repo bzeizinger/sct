37736195da79e16b35a27616f8a9fa32fcfbf5a7
bzeizinger <bennet.zeizinger@gmail.com>
2024-06-14 18:35:17 +0200

    Created link to GitHub

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ import webbrowser
 def openYoutube():
     webbrowser.open("https://youtube.com/@aidexe.mp4")
 
+def openGitHub():
+    webbrowser.open("https://github.com/bzeizinger")
+
 root = tk.Tk()
 root.title("SimpleComputeTools")
+root.geometry("500x250")
 
 links = ttk.Frame(root)
 links.pack()
@@ -17,5 +21,12 @@ ytLink.configure(foreground="#0000ff")
 ytLink.configure(cursor="hand")
 ytLink.grid(row=0, column=0)
 ytLink.bind("<Button-1>", openYoutube)
+
+ghLink = ttk.Label(links)
+ghLink.configure(text="GitHub")
+ghLink.configure(foreground="#0000ff")
+ghLink.configure(cursor="hand")
+ghLink.grid(row=0, column=1)
+ghLink.bind("<Button-1>", openGitHub)
 
 root.mainloop()
