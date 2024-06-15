a9b48c470fe85a005a4b85c5da8e692539ebc55d
bzeizinger <bennet.zeizinger@gmail.com>
2024-06-15 08:53:28 +0200

    Added the textfield for entering the content for the qrcode

--- QrCodeGenerator.py
+++ QrCodeGenerator.py
@@ -5,4 +5,10 @@ import qrcode
 def openQrCodeGenerator():
     qrCodeGeneratorWindow = tk.Tk()
     qrCodeGeneratorWindow.title("QR Code generator")
+
+    contentEntry = ttk.Entry(qrCodeGeneratorWindow)
+    contentEntry.configure(width=10)
+    contentEntry.pack()
+    contentEntry.insert(0, "Your content!")
+
     qrCodeGeneratorWindow.mainloop()
