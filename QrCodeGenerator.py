7ea06b5c99de4bdd181fc6658ae8cf415764bb61
bzeizinger <bennet.zeizinger@gmail.com>
2024-06-16 22:57:48 +0200

    Adding directions

--- QrCodeGenerator.py
+++ QrCodeGenerator.py
@@ -1,17 +1,20 @@
 import tkinter as tk
 from tkinter import ttk
 import qrcode
-from PIL import Image, ImageTk, 
+from PIL import Image, ImageTk
 
 def openQrCodeGenerator():
     qrCodeGeneratorWindow = tk.Tk()
     qrCodeGeneratorWindow.title("QR Code generator")
     qrCodeGeneratorWindow.geometry("500x250")
 
+    directions = ttk.Label(qrCodeGeneratorWindow)
+    directions.configure(text="Enter the content here:")
+    directions.pack()
+
     contentEntry = ttk.Entry(qrCodeGeneratorWindow)
     contentEntry.configure(width=100)
     contentEntry.pack()
-    contentEntry.insert(0, "Your content!")
 
     def createQrCode():
         qrcodeData = contentEntry.get()
@@ -19,6 +22,12 @@ def openQrCodeGenerator():
         if qrcodeData:
             qrcode.make(qrcodeData)
             qrcode.save("qrcode.png")
+
+            image = Image.open("qrcode.png")
+            photo = ImageTk.PhotoImage(image)
+
+            displayQrcode = ttk.Label(qrCodeGeneratorWindow)
+            displayQrcode.configure(image=photo)
 
             contentEntry.delete(0, tk.END)
 
